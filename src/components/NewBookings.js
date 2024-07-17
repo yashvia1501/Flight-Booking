773396ffd916752f2be5c855844fd22c91023072
Yashvi Agarwal <Yashvi.Agarwal@businessnext.com>
2024-07-17 04:08:25 -0700

    filter functionality by search and display

--- src/components/NewBookings.js
+++ src/components/NewBookings.js
@@ -1,32 +1,58 @@
-import React,{useState, useEffect} from 'react'
-import axios from 'axios';
+import React, { useState } from 'react';
+import { FaPlaneDeparture } from "react-icons/fa";
 import { value } from './Data';
 
 const NewBookings = () => {
-    const[post,setPosts]=useState(value)
-    const[originalpost,setoriginalPosts]=useState(value)
-    const[arrival,setarrival]=useState('')
-    const[dept,setDept]=useState('')
+  const [post, setPosts] = useState(value);
+  const [originalPost, setOriginalPosts] = useState(value);
+  const [arrival, setArrival] = useState('');
+  const [dept, setDept] = useState('');
 
-    const HandleInput=()=>{
+  const handleSearch = () => {
+    const updatedPost = originalPost.filter(item => 
+      item.arrival.toLowerCase().includes(arrival.trim().toLowerCase()) &&
+      item.departure.toLowerCase().includes(dept.trim().toLowerCase())
+    );
+    setPosts(updatedPost);
+  };
 
-    }
-    const HandleSearch=()=>{
-        const updatedpost= originalpost.filter(item=> item.arrival.toLowerCase().includes(arrival) || item.departure.toLowerCase().includes(dept))
-        setPosts(updatedpost)
-
-    }
-    console.log(post.length)
   return (
-    <div>
-      Arrival <input type='text' value={arrival} onChange={(e)=>setarrival(e.target.value)} />
-      Departure <input value={dept} onChange={(e)=>setDept(e.target.value)}/>
-      <button onClick={HandleSearch}>search</button>
+    <>
+      <div>
+        Arrival: <input type='text' value={arrival} onChange={(e) => setArrival(e.target.value)} />
+        Departure: <input value={dept} onChange={(e) => setDept(e.target.value)} />
+        Date: <input type='date' />
+        <div className="slidecontainer">
+          <p>Default range slider:</p>
+          {/* <input type="range" min={0} max={10000000} value={} onChange={updateFilterPrice}></input> */}
+        </div>
+        <button onClick={handleSearch}>Search</button>
 
-      
+        <div className='flight-details'>
+          {post.length > 0 ? post.map((item, index) => {
+            return (
+              <div key={index} style={{ display: "inline" }}>
+                  <div className='item-details'>
+                    <div className='item-name'>
+                      <FaPlaneDeparture style={{ fontSize: '20px', marginRight: '10px' }} />
+                      {item.name}
+                      <div className='item-rating'>{item.rating}</div>
+                      <div>{item.duration} hrs</div>
+                    </div>
+                    <div>{item.arrival} ---- {item.departure}</div>
+                    <div className='item-status'>
+                      <button style={{ backgroundColor: (item.status === 'Available Book Now' ? 'green' : item.status === 'Unavailable' ? 'red' : 'lightblue') }}>
+                        {item.status}
+                      </button>
+                    </div>
+                  </div>
+              </div>
+            )
+          }) : "null"}
+        </div>
+      </div>
+    </>
+  );
+};
 
-    </div>
-  )
-}
-
-export default NewBookings
+export default NewBookings;
